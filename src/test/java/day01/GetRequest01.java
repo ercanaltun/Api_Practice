bfa450e2a76769e8f59a52d7c2d14430adc6bd90
Ercan ALTUN <salsel0147@gmail.com>
2022-11-03 10:13:23 +0300

    03/11.

---
+++ src/test/java/day01/GetRequest01.java
@@ -0,0 +1,57 @@
+package day01;
+
+import io.restassured.response.Response;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.*;
+
+public class GetRequest01 {
+
+    @Test
+    public void test01() {
+
+        String url = "https://restful-booker.herokuapp.com/booking";
+
+        Response response = given().when().get(url);
+        //given().when().get(url) -> end point'e göndermek için request oluşturmuş olduk.
+        //Response response -> api tarafından bana dönen response (cevap)
+
+        // Response response = given().auth().basic("username", "password" ).when().get(url)
+        // basic auth ile request göndermek için
+
+        //response.prettyPrint();   ==> response'taki body'i yazdırır
+
+        //response.prettyPeek();    ==> response taki herşeyi yazdırır.
+
+       // response.peek();          ==> response taki headers 'ı vestring olarak body deki datayı verir
+
+        //response.print();         ==> string olarak body deki datayı verir
+        // [{"bookingid":1215},{"bookingid":844},{"bookingid":87},{"bookingid":747}, ...]
+
+        System.out.println(response.statusCode());
+        System.out.println(response.statusLine());
+        System.out.println(response.contentType());
+
+
+        // 1) JUnit Assert leri ile API testi yapabiliriz.
+        Assert.assertEquals("Status Kod Hatalı", 200, response.getStatusCode());
+        Assert.assertEquals("HTTP/1.1 200 OK", response.statusLine());
+        Assert.assertEquals("application/json; charset=utf-8", response.contentType());
+
+        // 2) assertThat ile
+        response.then().assertThat()
+                .statusCode(200)
+                .contentType("application/json; charset=utf-8")
+                .statusLine("HTTP/1.1 200 OK");
+    }
+
+}
+
+
+/*
+GithUb da target cakismamasi icin:
+1- Git ignore eklemek
+2- Target dosyasini silmek
+3- Terminal'de mvn clean
+ */
